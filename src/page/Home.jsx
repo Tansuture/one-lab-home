dca1d182d0fe9ba217ee4d8f1e26a6eba93c4505
Tansuture <tansuturemuratova@gmail.com>
2024-03-31 17:32:05 +0500

    add az

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,16 +1,39 @@
 import { Link } from "react-router-dom";
 import "./styles.css";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-    return (
-        <nav className="navigation-menu">
-            <Link to="/add">
-                Go to Add page
-            </Link>
-            <Link to="/list">
-                Go to List page
-            </Link>
-        </nav>
-    );
+  const [availabilityZone, setAvailabilityZone] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://169.254.169.254/latest/meta-data/placement/availability-zone",
+          {
+            headers: {
+              "X-aws-ec2-metadata-token":
+                "AQAEABUlqXs8wCetMEXYr40nRIOMT3HYihEJX1Bjya3Nl9az-SI8Aw==",
+            },
+          }
+        );
+        setAvailabilityZone(response.data);
+      } catch (error) {
+        console.error(
+          "There was an error fetching the availability zone:",
+          error
+        );
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <nav className="navigation-menu">
+      <div>{availabilityZone}</div>
+      <Link to="/add">Go to Add page</Link>
+      <Link to="/list">Go to List page</Link>
+    </nav>
+  );
 }
